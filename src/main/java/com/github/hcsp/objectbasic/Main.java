1b31d64307807fe3b111670add7639ed6d18c5a6
dingsheng <dingduoduo0916@gmail.com>
2019-06-29 11:25:17 +0800

    重载匹配多个时，强转

--- src/main/java/com/github/hcsp/objectbasic/Main.java
+++ src/main/java/com/github/hcsp/objectbasic/Main.java
@@ -5,7 +5,16 @@ import java.util.HashMap;
 public class Main {
     public static void main(String[] args) {
         // 请修复这里的编译错误，令实际调用的方法是print(HashMap)
-        print(null);
+        /**
+         * Object
+         *     -- Number
+         *          -- Integer
+         *     -- AbstractMap
+         *          -- HashMp
+         *
+         *                          print(null) 匹配了多个构造器， 需要强转类型
+         * */
+        print((HashMap)null);
     }
 
     public static void print(int i) {
