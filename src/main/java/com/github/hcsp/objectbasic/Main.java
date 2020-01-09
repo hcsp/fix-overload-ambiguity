9d753fc16428bd364d114f0857a42b4a9d3cd9d2
fiymi <fiymi666@gmail.com>
2020-01-09 09:10:04 +0800

    修复编译错误

--- src/main/java/com/github/hcsp/objectbasic/Main.java
+++ src/main/java/com/github/hcsp/objectbasic/Main.java
@@ -5,20 +5,22 @@ import java.util.HashMap;
 public class Main {
     public static void main(String[] args) {
         // 请修复这里的编译错误，令实际调用的方法是print(HashMap)
-        print(null);
+       print(null);
+//        Object i = 0;
+//        print(i);
     }
 
     public static void print(int i) {
         System.out.println("I'm int!");
     }
 
-    public static void print(Integer i) {
-        System.out.println("I'm Integer!");
-    }
+//    public static void print(Integer i) {
+//        System.out.println("I'm Integer!");
+//    }
 
-    public static void print(Number i) {
-        System.out.println("I'm Number!");
-    }
+//    public static void print(Number i) {
+//        System.out.println("I'm Number!");
+//    }
 
     public static void print(Object i) {
         System.out.println("I'm Object!");
