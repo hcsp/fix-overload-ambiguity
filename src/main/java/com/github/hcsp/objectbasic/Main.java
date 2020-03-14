3af7c58f159011217a05c5dc0c12fc7606fb5571
arsonalXxu <897386402@qq.com>
2020-03-14 18:54:33 +0800

    方法重载，最接近类型调用

--- src/main/java/com/github/hcsp/objectbasic/Main.java
+++ src/main/java/com/github/hcsp/objectbasic/Main.java
@@ -5,7 +5,8 @@ import java.util.HashMap;
 public class Main {
     public static void main(String[] args) {
         // 请修复这里的编译错误，令实际调用的方法是print(HashMap)
-        print(null);
+        HashMap<String, String> map = new HashMap<>();
+        print(map);
     }
 
     public static void print(int i) {
