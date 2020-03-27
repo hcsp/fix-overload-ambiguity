5cfe83628dfae17c57c12dd44e2a4e67a9131e90
何佳棋 <1149752574@qq.com>
2020-03-27 12:36:47 +0800

    修复重载方法中的歧义 (#169)
    修复重载方法中的歧义 (#169)

--- src/main/java/com/github/hcsp/objectbasic/Main.java
+++ src/main/java/com/github/hcsp/objectbasic/Main.java
@@ -2,29 +2,31 @@ package com.github.hcsp.objectbasic;
 
 import java.util.HashMap;
 
+import static java.lang.System.*;
+
 public class Main {
     public static void main(String[] args) {
         // 请修复这里的编译错误，令实际调用的方法是print(HashMap)
-        print(null);
+        print(((HashMap)null));
     }
 
     public static void print(int i) {
-        System.out.println("I'm int!");
+        out.println("I'm int!");
     }
 
     public static void print(Integer i) {
-        System.out.println("I'm Integer!");
+        out.println("I'm Integer!");
     }
 
     public static void print(Number i) {
-        System.out.println("I'm Number!");
+        out.println("I'm Number!");
     }
 
     public static void print(Object i) {
-        System.out.println("I'm Object!");
+        out.println("I'm Object!");
     }
 
     public static void print(HashMap i) {
-        System.out.println("I'm HashMap!");
+        out.println("I'm HashMap!");
     }
 }
