48e95d8a6ed4d8c6628777dc06d3f2124a3a20b2
jeffrey <908202572@qq.com>
2022-06-04 09:51:35 +0800

    修复重载方法中的歧义

--- src/main/java/com/github/hcsp/objectbasic/Main.java
+++ src/main/java/com/github/hcsp/objectbasic/Main.java
@@ -5,7 +5,7 @@ import java.util.HashMap;
 public class Main {
     public static void main(String[] args) {
         // 请修复这里的编译错误，令实际调用的方法是print(HashMap)
-        print(null);
+        print((HashMap<Object, Object>) null);
     }
 
     public static void print(int i) {
@@ -24,7 +24,7 @@ public class Main {
         System.out.println("I'm Object!");
     }
 
-    public static void print(HashMap i) {
+    public static void print(HashMap<Object, Object> i) {
         System.out.println("I'm HashMap!");
     }
 }
