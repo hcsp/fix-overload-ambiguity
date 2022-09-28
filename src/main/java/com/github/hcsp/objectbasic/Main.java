6dab84e62c8dd7c56c5a6c0a82e8bea772914d7f
wuchuan123 <609833577@qq.com>
2022-09-28 14:43:46 +0800

    作业 (#282)
    作业 (#282)

--- src/main/java/com/github/hcsp/objectbasic/Main.java
+++ src/main/java/com/github/hcsp/objectbasic/Main.java
@@ -3,28 +3,28 @@ package com.github.hcsp.objectbasic;
 import java.util.HashMap;
 
 public class Main {
-    public static void main(String[] args) {
-        // 请修复这里的编译错误，令实际调用的方法是print(HashMap)
-        print(null);
-    }
+  public static void main(String[] args) {
+    // 请修复这里的编译错误，令实际调用的方法是print(HashMap)
+    print((HashMap) null);
+  }
 
-    public static void print(int i) {
-        System.out.println("I'm int!");
-    }
+  public static void print(int i) {
+    System.out.println("I'm int!");
+  }
 
-    public static void print(Integer i) {
-        System.out.println("I'm Integer!");
-    }
+  public static void print(Integer i) {
+    System.out.println("I'm Integer!");
+  }
 
-    public static void print(Number i) {
-        System.out.println("I'm Number!");
-    }
+  public static void print(Number i) {
+    System.out.println("I'm Number!");
+  }
 
-    public static void print(Object i) {
-        System.out.println("I'm Object!");
-    }
+  public static void print(Object i) {
+    System.out.println("I'm Object!");
+  }
 
-    public static void print(HashMap i) {
-        System.out.println("I'm HashMap!");
-    }
+  public static void print(HashMap i) {
+    System.out.println("I'm HashMap!");
+  }
 }
